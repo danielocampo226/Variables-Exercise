f73ad7040cd2640b13b150f96798d36e24f0e5eb
danielocampo226 <sierradanny562@gmail.com>
2023-02-13 18:12:23 -0500

    variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const string name = "John";
+            int age = 31;
+            char middleInitial = 'D';
+            bool isOver18 = true;
+            double currentTemp = 77.9;
+            decimal currentPrice = 29.99m;
+            
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");   
+            Console.WriteLine($"middleInitial: {middleInitial}");
+            Console.WriteLine($"IsOver18: {isOver18}");
+            Console.WriteLine($"currentTemp: {currentTemp}");
+            Console.WriteLine($"currentPrice: {currentPrice}");
+
         }
     }
 }
